677799fac0c2c5d91bfbdc69b1bef3694251a356
AlvaroL <alvaro.llorente.ramos@gmail.com>
2017-09-18 20:58:24 +0200

    First dummy implementation for login mock

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,5 +1,37 @@
+const mockPostLogin = (request) => {
+    const body = request.body;
+    if (body.user && body.user.indexOf('@ing.com') !== -1) {
+        // valid email, get correct json
+        return {
+            status: 200,
+            body: {
+                token: 'jksdq67yehq2ui3y93ih'
+            }
+        };
+    } else {
+        // invalid email
+        return {
+            status: 400,
+            body: {
+                message: 'Incorrect user or password'
+            }
+        };
+    }
+};
+
+const mockGetLogin = (request) => {
+    
+};
+
+const METHOD_FACTORY = {
+    'GET': mockGetLogin,
+    'POST': mockPostLogin
+};
+
 module.exports = {
     login : (ctx) => {
-        ctx.body = 'login!';
+        const response = METHOD_FACTORY[ctx.request.method](ctx.request);
+        ctx.body = response.body;
+        ctx.status = response.status;
     }
 };
